3a7795f5f6a100fb9ad0552114c155dc494cd5a4
thixphix <sophiamolina1201@gmail.com>
2023-06-10 19:46:09 -0400

    cambios

--- creandoMarcoCentrado.java
+++ creandoMarcoCentrado.java
@@ -1,28 +1,37 @@
 package graficos;
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Image;
 import javax.swing.JFrame;
 
-//Sophia Molina 28.470.485
+
+// Sophia Molina 28.470.485
+
 
 public class creandoMarcoCentrado {
+	
 
 	public static void main(String[] args) {
 		marcoCentrado miMarco = new marcoCentrado();
 		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		miMarco.setVisible(true);
+		miMarco.setTitle("computacion Grafica");
 	}
 
 }
 
 class marcoCentrado extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
+
 	public marcoCentrado() {
 		Toolkit miPantalla = Toolkit.getDefaultToolkit();
-		Dimension tama�oPantalla = miPantalla.getScreenSize();
-		int alturaPantalla = tama�oPantalla.height;
-		int anchoPantalla = tama�oPantalla.width;
+		Dimension tamañoPantalla = miPantalla.getScreenSize();
+		int alturaPantalla = tamañoPantalla.height;
+		int anchoPantalla = tamañoPantalla.width;
 		setSize(anchoPantalla/2, alturaPantalla/2);
 		setLocation(anchoPantalla/4, alturaPantalla/4);
+		Image miIcono = miPantalla.getImage("Images.png");
+		setIconImage(miIcono);
 	}
 }
